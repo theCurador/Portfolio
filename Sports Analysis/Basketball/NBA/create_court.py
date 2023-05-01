21bd8b6bd17aab229f244a716841d66b1cfe79aa
J.Zayas <117095630+theCurador@users.noreply.github.com>
2023-05-01 08:52:47 -0400

    Goat Results

--- Sports Analysis/Basketball/NBA/create_court.py
+++ Sports Analysis/Basketball/NBA/create_court.py
@@ -40,3 +40,45 @@ fig = plt.figure(figsize=(4, 3.76))
 ax = fig.add_axes([0, 0, 1, 1])
 ax = create_court(ax, 'black')
 plt.show()
+
+
+
+def create_court2(color='black', lw=2, outer_lines=False):
+
+    # Short corner 3pt lines 
+    plt.plot([-220, -220], [0, 140], linewidth=lw, color=color)
+    plt.plot([220, 220], [0, 140], linewidth=lw, color=color)
+    
+    # 3PT Arc
+    plt.add_artist(mpl.patches.Arc((0, 140), 440, 315, theta1=0, theta2=180, facecolor='none', edgecolor=color, lw=lw))
+    
+    # Lane and Key
+    plt.plot([-80, -80], [0, 190], linewidth=lw, color=color)
+    plt.plot([80, 80], [0, 190], linewidth=lw, color=color)
+    plt.plot([-60, -60], [0, 190], linewidth=lw, color=color)
+    plt.plot([60, 60], [0, 190], linewidth=lw, color=color)
+    plt.plot([-80, 80], [190, 190], linewidth=lw, color=color)
+    plt.add_artist(mpl.patches.Circle((0, 190), 60, facecolor='none', edgecolor=color, lw=lw))
+    
+    # Rim
+    plt.add_artist(mpl.patches.Circle((0, 60), 15, facecolor='none', edgecolor=color, lw=lw))
+    
+    # Backboard
+    plt.plot([-30, 30], [40, 40], linewidth=lw, color=color)
+
+    if outer_lines:
+        # Outer box of the basketball court
+        plt.plot([-250, 250], [-47.5, -47.5], linewidth=lw, color=color)
+        plt.plot([-250, 250], [470.5, 470.5], linewidth=lw, color=color)
+        plt.plot([-250, -250], [-47.5, 470.5], linewidth=lw, color=color)
+        plt.plot([250, 250], [-47.5, 470.5], linewidth=lw, color=color)
+
+    # Remove ticks
+    plt.xticks([])
+    plt.yticks([])
+
+    # Set axis limits
+    plt.xlim(-250, 250)
+    plt.ylim(0, 470)
+
+    return plt.gca()
