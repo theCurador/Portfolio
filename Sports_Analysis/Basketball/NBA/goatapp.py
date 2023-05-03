f8f38ba67dd3388abf38be7a7b6aa5afa5d44032
J.Zayas <117095630+theCurador@users.noreply.github.com>
2023-05-02 21:46:53 -0400

    mean

--- Sports_Analysis/Basketball/NBA/goatapp.py
+++ Sports_Analysis/Basketball/NBA/goatapp.py
@@ -8,14 +8,14 @@ results_df = pd.read_csv('/Users/jeanzayas/Desktop/Divergence/Portfolio/Sports_A
 def display_boxscore(game_index):
     sl.write(f"### Game {game_index+1} Box Score")
     sl.write("Lebron")
-    sl.write(f"Points: {results_df.iloc[game_index]['Lebron(PPG)']}")
+    sl.write(f"Points: {results_df.iloc[game_index]['Lebron Pts']}")
     sl.write(f"Rebounds: {results_df.iloc[game_index]['LJ Total Reb']}")
     sl.write(f"3-Pts: {results_df.iloc[game_index]['LeBron 3PPGG']}")
     sl.write(f"Blocks: {results_df.iloc[game_index]['LeBron Blocks']}")
     sl.write(f"Steals: {results_df.iloc[game_index]['LeBron Steals']}")
     sl.write("")
     sl.write("Jordan")
-    sl.write(f"Points: {results_df.iloc[game_index]['Jordan (PPG)']}")
+    sl.write(f"Points: {results_df.iloc[game_index]['MJ pts']}")
     sl.write(f"Rebounds: {results_df.iloc[game_index]['MJ Total Reb']}")
     sl.write(f"3-Pointers: {results_df.iloc[game_index]['Jordan 3PPG']}")
     sl.write(f"Blocks: {results_df.iloc[game_index]['Jordan Blocks']}")
@@ -25,13 +25,13 @@ def display_boxscore(game_index):
 def display_recap():
     sl.write("### Overall Recap")
     sl.write(f"Simulated Games: {results_df.shape[0]}")
-    sl.write(f"LeBron wins: {results_df['LeBron wins:'].sum()}")
-    sl.write(f"Jordan wins: {results_df['Jordan wins:'].sum()}")
+    sl.write(f"LeBron wins: {results_df['LeBron wins:'].mean()}")
+    sl.write(f"Jordan wins: {results_df['Jordan wins:'].mean()}")
     sl.write(f"LJ WIN prob: {results_df['LJ WIN prob'].mean():.2%}")
     sl.write(f"MJ WIN prob: {results_df['MJ WIN prob'].mean():.2%}")
     
 # Add a search bar for game number 
-game_number = sl.text_input("Enter a game number between 1 and 20,000:", "1")
+game_number = sl.text_input("Enter a game number (1 and 20,000) :", "1")
 try: 
     game_index = int(game_number) - 1 # Convert to 0- based index
     if game_index >= 0 and game_index < results_df.shape[0]:
